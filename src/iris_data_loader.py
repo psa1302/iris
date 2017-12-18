e58dcaf3daf5273bda7ceb2af81a7df89467c619
Puneet Saini <sainipuneet@live.in>
2017-12-18 15:08:22 +0530

    Fixed iris_data_loader

--- src/iris_data_loader.py
+++ src/iris_data_loader.py
@@ -1,42 +1,36 @@
+# Libraries
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
 def load_data():
+
+	"""Load the Iris dataset and assign proper variables after splitting it
+	into test data and validation data with 80% of data being used to test and 
+	20% data used for validation."""
+
 	url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
 	names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
-	dataset = pd.read_csv(url, names=names)
+	dataset = pd.read_csv(url, names=names).replace({'class': {'Iris-setosa': 0, 'Iris-versicolor': 1, 'Iris-virginica': 2}})
 	training_data, validation_data = train_test_split(dataset, test_size=0.2)
-	test_data = dataset
 	return (training_data, validation_data, dataset)
 
-def array_conv(a, col1, col2):
-	arr = [pd.DataFrame(a, columns=col1).values, pd.DataFrame(a, columns=col2).values]
-	return arr
+def vectorized_result(j):
+
+	"""To vectorize the ouput."""
+	
+	z = np.zeros((3, 1))
+	z[int(j)] = 1.0
+	return z
 
 def load_data_wrapper():
 	tr_d, va_d, te_d = load_data()
-	class_data = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width']
-	train_d = array_conv(tr_d, class_data, ['class'])
-	valid_d = array_conv(va_d, class_data, ['class'])
-	test_d = array_conv(te_d, class_data, ['class'])
-	training_inputs = [np.reshape(x, (4, 1)) for x in train_d[0]]
-	training_results = [vectorized_result(y) for y in train_d[1]]
-	training_data = zip(training_inputs, training_results)
-	validation_inputs = [np.reshape(x, (4, 1)) for x in valid_d[0]]
-	validation_data = zip(validation_inputs, valid_d[1])
-	test_inputs = [np.reshape(x, (4, 1)) for x in test_d[0]]
-	test_data = zip(test_inputs, test_d[1])
-	return (training_data, validation_data, test_data)
-
-def vectorized_result(j):
-	e = np.zeros((10, 1))
-	if j == 'Iris-sentosa':
-		e[0] = 1.0
-	elif j == 'Iris-versicolor':
-		e[1] = 1.0
-	else:
-		e[2] = 1.0
-	return e
-
-load_data_wrapper()
+	training_d = np.split(tr_d, [4], axis=1)
+	validation_d = np.split(va_d, [4], axis=1)
+	training_inputs = [np.reshape(x, (4,1)) for x in training_d[0].values]
+	training_results = [vectorized_result(y) for y in training_d[1].values]
+	training_data = list(zip(training_inputs, training_results))
+	validation_inputs = [np.reshape(x, (4, 1)) for x in validation_d[0].values]
+	validation_results = [vectorized_result(y) for y in validation_d[1].values]
+	validation_data = list(zip(validation_inputs, validation_results))
+	return (training_data, validation_data)
